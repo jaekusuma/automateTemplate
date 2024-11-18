53f6645d532bf1a896dd69cc79890b38c00be593
jaekusuma <jaekusuma@gmail.com>
2024-11-18 15:30:29 +0700

    move setting page url in serenity.conf

--- src/test/java/pages/urlLinkSite.java
+++
@@ -1,8 +0,0 @@
-package pages;
-
-import net.serenitybdd.annotations.DefaultUrl;
-import net.thucydides.core.pages.PageObject;
-
-@DefaultUrl("https://www.google.com/")
-public class urlLinkSite extends PageObject {
-}
--- src/test/java/stepDef/stepDefs.java
+++ src/test/java/stepDef/stepDefs.java
@@ -6,14 +6,14 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.actions.Open;
 import net.serenitybdd.screenplay.ensure.Ensure;
 import net.serenitybdd.screenplay.questions.page.TheWebPage;
-import pages.rule;
 
 public class stepDefs {
     @Given("{actor} Open google")
     public void openGoogle(Actor actor) {
-        actor.wasAbleTo(rule.openWeb());
+        actor.attemptsTo(Open.browserOn().thePageNamed("page.google"));
     }
 
 
