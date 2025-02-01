9787e260426c96af3f2dcee0680fb6d266681dce
jaekusuma <jaekusuma@gmail.com>
2025-02-01 21:55:02 +0700

    add readme, but im too lazy to edit :)

---
+++ README.md
@@ -0,0 +1,4 @@
+it just quickstart for testing with selenium with java language.
+using serenity and cucumber format
+
+include web and api.
--- src/test/java/runTest.java
+++ src/test/java/runTest.java
@@ -1,13 +1,11 @@
-import org.junit.platform.suite.api.ConfigurationParameter;
-import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasspathResource;
-import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.*;
 
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("/features")
+@IncludeTags("DuckDuckGo")
 @ConfigurationParameter(
         key = PLUGIN_PROPERTY_NAME,
         value = "io.cucumber.core.plugin.SerenityReporterParallel,pretty,timeline:build/test-results/timeline"
